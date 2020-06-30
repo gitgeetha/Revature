b5f0b9aad7d0f5f7787344a306f7486d4923a20a
gitgeetha <write2ggc@gmail.com>
2020-06-29 23:29:37 -0500

    Java Prg

---
+++ JavaHello.java
@@ -0,0 +1,31 @@
+public class JavaHello{
+        public static void main(String...args){
+                System.out.println("Hellooooo world!");
+
+        }
+}
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+HelloWorld.java [unix] (23:16 29/06/2020)                          3,30-44 All
+"HelloWorld.java" [unix] 6L, 115C
+
